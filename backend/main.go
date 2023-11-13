9c7487718deaf7da2bf85d075dd00760158b1c2b
Felix Maaß <github@flixma.de>
2023-11-13 09:12:38 +0100

    More descriptive error codes + introduce constants + add inline docs/comments

--- backend/main.go
+++ backend/main.go
@@ -22,17 +22,19 @@ import (
 	"github.com/go-webauthn/webauthn/webauthn"
 )
 
-var (
-	webAuthn         *webauthn.WebAuthn
-	err              error
-	webAuthnSessions map[string]*webauthn.SessionData
+const (
+	WEBAUTHN_COLLECTION_NAME       string = "users"
+	WEBAUTHN_CREDENTIALS_FIELDNAME string = "webauthn_credentials"
+	WEBAUTHN_ID_B64_FIELDNAME      string = "webauthn_id_b64"
 )
 
 func findUser(app *pocketbase.PocketBase, username string) (*User, error) {
 	// Find user
 	user := User{}
 	err := app.Dao().DB().
-		NewQuery("SELECT id, username, name, webauthn_id_b64, webauthn_credentials FROM users WHERE username={:username}").
+		NewQuery(fmt.Sprintf(
+			"SELECT id, username, name, %s, %s FROM %s WHERE username={:username}",
+			WEBAUTHN_ID_B64_FIELDNAME, WEBAUTHN_CREDENTIALS_FIELDNAME, WEBAUTHN_COLLECTION_NAME)).
 		Bind(dbx.Params{"username": username}).
 		One(&user)
 	if err != nil {
@@ -45,7 +47,7 @@ func findUser(app *pocketbase.PocketBase, username string) (*User, error) {
 
 func createUser(app *pocketbase.PocketBase, username string) error {
 
-	collection, err := app.Dao().FindCollectionByNameOrId("users")
+	collection, err := app.Dao().FindCollectionByNameOrId(WEBAUTHN_COLLECTION_NAME)
 	if err != nil {
 		return err
 	}
@@ -59,11 +61,11 @@ func createUser(app *pocketbase.PocketBase, username string) error {
 	password := base64.StdEncoding.EncodeToString(randomBuffer)
 
 	err = form.LoadData(map[string]any{
-		"username":             username,
-		"password":             password,
-		"passwordConfirm":      password,
-		"webauthn_id_b64":      "",
-		"webauthn_credentials": "",
+		"username":                     username,
+		"password":                     password,
+		"passwordConfirm":              password,
+		WEBAUTHN_ID_B64_FIELDNAME:      "",
+		WEBAUTHN_CREDENTIALS_FIELDNAME: "",
 	})
 	if err != nil {
 		return err
@@ -75,33 +77,38 @@ func createUser(app *pocketbase.PocketBase, username string) error {
 
 func findOrCreateUser(app *pocketbase.PocketBase, username string) (*User, error) {
 
+	// try to find user
 	user, err := findUser(app, username)
 
-	// create user if not existent
+	// error? -> create user if not existent
 	if err != nil {
 		err = createUser(app, username)
 		if err != nil {
-			return user, err
+			return nil, err
 		}
 		user, err = findUser(app, username)
 		if err != nil {
-			return user, err
+			return nil, err
 		}
 	}
 
 	err = user.ensureWebAuthnId(app)
-	return user, err
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (user *User) ensureWebAuthnId(app *pocketbase.PocketBase) error {
-	// create webauthn id only if it doesnt exist yet
-	if user.WebAuthnIdB64 != "" {
-		return nil
+	authRecord, err := app.Dao().FindAuthRecordByUsername(WEBAUTHN_COLLECTION_NAME, user.Username)
+	if err != nil {
+		return apis.NewNotFoundError("User not found.", err)
 	}
 
-	authRecord, err := app.Dao().FindAuthRecordByUsername("users", user.Username)
-	if err != nil {
-		return apis.NewBadRequestError("user not found.", err)
+	// create webauthn id only if it doesnt exist yet
+	if authRecord.GetString(WEBAUTHN_ID_B64_FIELDNAME) != "" {
+		return nil
 	}
 
 	// create 64 bytes of random data
@@ -110,33 +117,33 @@ func (user *User) ensureWebAuthnId(app *pocketbase.PocketBase) error {
 	user.WebAuthnIdB64 = base64.StdEncoding.EncodeToString(randomBuffer)
 
 	// store in database
-	authRecord.Set("webauthn_id_b64", user.WebAuthnIdB64)
+	authRecord.Set(WEBAUTHN_ID_B64_FIELDNAME, user.WebAuthnIdB64)
 	err = app.Dao().Save(authRecord)
 	if err != nil {
-		return apis.NewBadRequestError("could not store webauthn id to db.", err)
+		return NewInternalServerError("Could not store webauthn id to db.", err)
 	}
 
 	return nil
 }
 
 func (user User) addCredential(app *pocketbase.PocketBase, credential webauthn.Credential) error {
-	authRecord, err := app.Dao().FindAuthRecordByUsername("users", user.Username)
+	authRecord, err := app.Dao().FindAuthRecordByUsername(WEBAUTHN_COLLECTION_NAME, user.Username)
 	if err != nil {
-		return apis.NewBadRequestError("user not found.", err)
+		return apis.NewNotFoundError("User not found.", err)
 	}
-	authRecord.Set("webauthn_credentials", credential)
+	authRecord.Set(WEBAUTHN_CREDENTIALS_FIELDNAME, credential)
 	return app.Dao().Save(authRecord)
 }
 
 func (user User) sendAuthTokenResponse(app *pocketbase.PocketBase, c echo.Context) error {
 
-	authRecord, err := app.Dao().FindAuthRecordByUsername("users", user.Username)
+	authRecord, err := app.Dao().FindAuthRecordByUsername(WEBAUTHN_COLLECTION_NAME, user.Username)
 	if err != nil {
-		return apis.NewBadRequestError("User not found.", err)
+		return apis.NewNotFoundError("User not found.", err)
 	}
 	token, err := tokens.NewRecordAuthToken(app, authRecord)
 	if err != nil {
-		return apis.NewBadRequestError("Failed to create auth token.", err)
+		return NewInternalServerError("Failed to create auth token.", err)
 	}
 
 	return c.JSON(http.StatusOK, map[string]any{
@@ -145,29 +152,42 @@ func (user User) sendAuthTokenResponse(app *pocketbase.PocketBase, c echo.Contex
 	})
 }
 
+func NewInternalServerError(message string, data any) *apis.ApiError {
+	return apis.NewApiError(500, message, data)
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////        MAIN        //////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func main() {
 
+	// configure and initialize webauthn
 	wconfig := &webauthn.Config{
 		RPDisplayName: "Felix' PB Webauthn",                                       // Display Name for your site
 		RPID:          "localhost",                                                // Generally the FQDN for your site
 		RPOrigins:     []string{"http://localhost:8090", "http://localhost:5173"}, // The origin URLs allowed for WebAuthn requests
 	}
 
-	if webAuthn, err = webauthn.New(wconfig); err != nil {
+	webAuthn, err := webauthn.New(wconfig)
+	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	webAuthnSessions = make(map[string]*webauthn.SessionData)
 
+	// create a map holding the sessions used during registration and login flow
+	webAuthnSessions := make(map[string]*webauthn.SessionData)
+
+	// Configure the pocketbase server
 	app := pocketbase.New()
-
-	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+
+		// serves static files from the provided public dir (if exists)
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 
+		// register routes for registration:
+		// 	1. *begin* creates a challenge for the authenticator to sign
+		// 	2. *finish* validates the generated credential and stores it in the user record
 		e.Router.POST("/webauthn-begin-registration/:userb64", func(c echo.Context) error {
 			usernameb64 := c.PathParam("userb64")
 			username, err := base64.StdEncoding.DecodeString(usernameb64)
@@ -175,18 +195,16 @@ func main() {
 				return apis.NewBadRequestError("Could not decode user from path.", err)
 			}
 
-			data := apis.RequestInfo(c).Data
-			fmt.Printf("/webauthn-begin-registration data: %v\n", data)
-
 			// Find or create the new user
+			// TODO: if the user exists, throw an error (cant register a credential for existing users, if not authenticated)
 			user, err := findOrCreateUser(app, string(username))
 			if err != nil {
-				return apis.NewBadRequestError("User not found.", err)
+				return apis.NewNotFoundError("User not found.", err)
 			}
 
 			options, session, err := webAuthn.BeginRegistration(user)
 			if err != nil {
-				return err
+				return NewInternalServerError("Could not start WebAuthn registration flow.", err)
 			}
 
 			// store the sessionData values
@@ -204,13 +222,10 @@ func main() {
 				return apis.NewBadRequestError("Could not decode user from path.", err)
 			}
 
-			data := apis.RequestInfo(c).Data
-			fmt.Printf("/webauthn-finish-registration data: %v\n", data)
-
 			// Find or create the new user
 			user, err := findOrCreateUser(app, string(username))
 			if err != nil {
-				return apis.NewBadRequestError("User not found.", err)
+				return apis.NewNotFoundError("User not found.", err)
 			}
 			session := webAuthnSessions[user.WebAuthnIdB64]
 			delete(webAuthnSessions, user.WebAuthnIdB64)
@@ -230,6 +245,9 @@ func main() {
 			return c.JSON(http.StatusOK, map[string]string{"status": "success"})
 		})
 
+		// register routes for authentication (similar to registration):
+		// 	1. *begin* creates a challenge for the authenticator to sign
+		// 	2. *finish* validates the signature and responds with a pocketbase auth token
 		e.Router.POST("/webauthn-begin-login/:userb64", func(c echo.Context) error {
 			usernameb64 := c.PathParam("userb64")
 			username, err := base64.StdEncoding.DecodeString(usernameb64)
@@ -237,12 +255,9 @@ func main() {
 				return apis.NewBadRequestError("Could not decode user from path.", err)
 			}
 
-			data := apis.RequestInfo(c).Data
-			fmt.Printf("/webauthn-begin-login data: %v\n", data)
-
 			user, err := findUser(app, string(username))
 			if err != nil {
-				return apis.NewBadRequestError("User not found.", err)
+				return apis.NewNotFoundError("User not found.", err)
 			}
 
 			options, session, err := webAuthn.BeginLogin(user)
@@ -267,25 +282,19 @@ func main() {
 				return apis.NewBadRequestError("Could not decode user from path.", err)
 			}
 
-			data := apis.RequestInfo(c).Data
-			fmt.Printf("/webauthn-finish-login data: %v\n", data)
-
 			user, err := findOrCreateUser(app, string(username))
 			if err != nil {
-				return apis.NewBadRequestError("User not found.", err)
+				return apis.NewNotFoundError("User not found.", err)
 			}
 
-			// Get the session data stored from the function above
+			// Get the session data stored from the function above and complete login
 			session := webAuthnSessions[user.WebAuthnIdB64]
-
 			_, err = webAuthn.FinishLogin(user, *session, c.Request())
 			if err != nil {
-				// Handle Error and return.
-
 				return apis.NewBadRequestError("Failed to verify login credentials.", err)
 			}
 
-			// If login was successful, handle next steps
+			// If login was successful, send auth token for further communication
 			return user.sendAuthTokenResponse(app, c)
 		})
 
